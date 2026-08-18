c481dde43ff054a06d471f47fccdbf298dde8d0f
kmshariat <cast.shariat@gmail.com>
2026-08-18 14:37:55 +0600

    fixed init file

---
+++ tiltr/__init__.py
@@ -0,0 +1,19 @@
+from .tiltr import (
+    Plotter,
+    lambda_T,
+    lambda_a,
+    lambda_M,
+    psi_T,
+    psi_a,
+    psi_M,
+)
+
+__all__ = [
+    'Plotter',
+    'lambda_T',
+    'lambda_a',
+    'lambda_M',
+    'psi_T',
+    'psi_a',
+    'psi_M',
+]
